9c31d745e204a84be549c8f9d6f3a2ba73a29762
ajbraus <ajbraus@gmail.com>
2013-08-05 15:55:39 -0500

    blah

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130804224600) do
+ActiveRecord::Schema.define(:version => 20130805204632) do
 
   create_table "activities", :force => true do |t|
     t.integer  "trackable_id"
@@ -249,8 +249,13 @@ ActiveRecord::Schema.define(:version => 20130804224600) do
     t.string   "linked_in_url"
     t.string   "location"
     t.boolean  "terms",                  :default => false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.boolean  "admin",                  :default => false
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 

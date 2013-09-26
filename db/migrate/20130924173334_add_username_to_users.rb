dfa0c1397e103bf35d8fb195d6790492326c6b0b
ajbraus <ajbraus@gmail.com>
2013-09-26 15:57:52 -0500

    blah

--- db/migrate/20130924173334_add_username_to_users.rb
+++ db/migrate/20130924173334_add_username_to_users.rb
@@ -2,10 +2,10 @@ class AddUsernameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :username, :string
   end
-
-  User.all.each do |u|
-    u.username = u.first_name_with_last_initial.split.join('-')[0..-2]
-    u.save
-  end
 end
+
+# User.all.each do |u|
+#   u.username = u.first_name_with_last_initial.split.join('-')[0..-2]
+#   u.save
+# end
 

dd32a699aa3ba445e0147aeb6f358500494958db
ajbraus <ajbraus@gmail.com>
2013-12-11 16:43:57 -0600

    blah"

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,6 +1,5 @@
 class InvitationsController < Devise::InvitationsController
   def create
-
     params[:user][:email].split(',').each do |email|    
       User.invite!(email: email) do |u|
         u.skip_invitation = true
@@ -9,7 +8,6 @@ class InvitationsController < Devise::InvitationsController
           format.html
           format.js
         end
-        return
       end
     end
 

5c3c1b014c0ba839f82b712ff638dc04b82486c5
ajbraus <ajbraus@gmail.com>
2013-12-31 14:21:07 -0600

    blah

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   include PublicActivity::StoreController
 
+  # before_filter :check_password_present
+
+  # def check_password_present
+  #   if user_signed_in?
+  #     if current_user.password.blank?
+  #       edit_password_path(current_user)
+  #     end
+  #   end
+  # end
+
   protect_from_forgery
 end

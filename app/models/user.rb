092e8d0d6e91e23a938a38a8575b09a3880ef564
ajbraus <ajbraus@gmail.com>
2014-04-26 16:59:21 -0500

    fixing

--- app/models/user.rb
+++ app/models/user.rb
@@ -140,7 +140,7 @@ class User < ActiveRecord::Base
   def set_products
     if !bank? && finished_profile?
       self.products = []
-      if loan_sizes.first.rank < 3
+      if loan_sizes.first.rank < 2
         self.products << Product.find_by_name("Deposits") 
         self.products << Product.find_by_name("Credit Cards") 
       end

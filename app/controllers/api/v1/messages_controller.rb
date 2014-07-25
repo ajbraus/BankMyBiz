c1e8bdfb22c7c06da77eeb82693da094829845f9
Adam Braus <ajbraus@gmail.com>
2014-07-25 16:40:35 -0500

    fixng

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -4,12 +4,6 @@ class Api::V1::MessagesController < ApplicationController
   def index
     @messages = current_user.messages.order('created_at desc')
     @sent_messages = current_user.sent_messages.order('created_at desc')
-    #interlocutor_ids = Message.where("sender_id = ? OR receiver_id = ?", current_user, current_user).order('created_at desc').uniq
-    interlocutor_ids = @messages.pluck(:receiver_id) | @sent_messages.pluck(:sender_id)
-
-    Message.where("sender_id = ? OR receiver_id = ?", current_user, current_user).order('created_at desc').remove do |m|
-
-    end
 
     render 'api/messages/index'
   end
@@ -21,7 +15,7 @@ class Api::V1::MessagesController < ApplicationController
     else
       other_user = @message.sender
     end
-    @messages = Message.where("(sender_id = :current_user_id OR sender_id = :other_user_id) AND (receiver_id = :current_user_id OR receiver_id = :other_user_id)", current_user_id: current_user.id, other_user_id: other_user.id)
+    @messages = Message.where("(sender_id = :current_user_id OR sender_id = :other_user_id) AND (receiver_id = :current_user_id OR receiver_id = :other_user_id)", current_user_id: current_user.id, other_user_id: other_user.id).order('created_at DESC')
 
     render 'api/messages/show'
   end

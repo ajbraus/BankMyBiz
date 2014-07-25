ca7d511cdf431944b6462f75f15d93066e3432a0
Adam Braus <ajbraus@gmail.com>
2014-07-25 14:50:15 -0500

    fixingg

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,10 @@ class Api::V1::UsersController < ApplicationController
   before_filter :authenticate_user_from_token!
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(auth_token: params[:id])
+    if @user.blank?
+      @user = User.find(params[:id])
+    end
 
     render 'api/users/show'
   end
